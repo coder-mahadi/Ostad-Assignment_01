b283ccf7a8960ee4585b499a6b990ad957079d82
coder-mahadi <mksbd.net@gmail.com>
2022-11-07 00:43:15 +0600

    Change and add

--- index.js
+++ index.js
@@ -2,16 +2,33 @@ import {
     countries,
 }
 from './countries.js';
+
+
+// Travers Full array
 countries.map((list, i) => {
-    let checkCountry = "Bangladesh";
-    if (list.name === checkCountry) {
-        console.log(`Country is found`);
-        console.log(`Country is ${checkCountry} code: ${list.code}`)
+    // console.log(`Listed country is ${list.name}`)
+    // console.log(`Listed country code is ${list.code}`)
+})
+
+
+// Country check and added
+for (let item of countries) {
+    let checkCountry = "Viatenam";
+    if (item.name === checkCountry) {
+        // console.log(`Country is found`);
+        break;
     } else {
         countries.push({
             name: checkCountry,
             "code": checkCountry.slice(0, 3).toUpperCase()
         })
-        console.log(countries);
+        // console.log(countries);
+        break;
     }
-})
+}
+
+// Sort Full Array Object
+countries.sort(
+    (p1, p2) =>
+    (p1.name > p2.name) ? 1 : (p1.name < p2.name) ? -1 : 0);
+console.log(countries)
